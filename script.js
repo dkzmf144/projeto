dbe67105ea4bb0450926708f8005399d72db1290
dkzmf144 <dkzmf144@gmail.com>
2025-02-27 22:21:39 -0300

    update script.js

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function toggleMenu() {
     menu.classList.toggle('open');
     content.classList.toggle('shifted');
 }
+
+// Função para fechar o menu ao clicar fora
+document.addEventListener('click', function(event) {
+    const menu = document.getElementById('menu');
+    const menuToggleButton = document.getElementById('menu-toggle');
+    
+    // Verifica se o clique foi fora do menu e do botão de menu
+    if (!menu.contains(event.target) && event.target !== menuToggleButton) {
+        menu.classList.remove('open');
+        document.getElementById('content').classList.remove('shifted');
+    }
+});
